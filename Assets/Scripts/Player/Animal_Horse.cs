045e85f5977b56bec6b51e5a9c6bc51e36c88782
loffeat <51206918+loffeat@users.noreply.github.com>
2026-01-31 23:52:20 +0800

    new animals committed

---
+++ Assets/Scripts/Player/Animal_Horse.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Animal_Horse : Animal
+{
+    public Transform shitPoint;
+    public GameObject shitPrefab;
+    public MMF_Player ShitMMF;
+    [Tooltip("屎飞出的力（Impulse）")]
+    public float shitForce = 0.5f;
+
+    public override bool DoBehavior1()
+    {
+        if (!base.DoBehavior1()) return false;
+        ShitMMF?.PlayFeedbacks();
+        Vector3 spawnPos;
+        Vector2 awayDir;
+        if (animalSprite != null && animalSprite.flipX)
+        {
+            spawnPos = new Vector3(2f * transform.position.x - shitPoint.position.x, shitPoint.position.y, shitPoint.position.z);
+            awayDir = ((Vector2)spawnPos - (Vector2)transform.position).normalized;
+        }
+        else
+        {
+            spawnPos = shitPoint.position;
+            awayDir = ((Vector2)spawnPos - (Vector2)transform.position).normalized;
+        }
+
+        GameObject shit = Instantiate(shitPrefab, spawnPos, Quaternion.identity);
+        var rb = shit.GetComponent<Rigidbody2D>();
+        if (rb != null)
+            rb.AddForce(-awayDir * shitForce, ForceMode2D.Impulse);
+
+        return true;
+    }
+
+    public override bool DoBehavior2()
+    {
+        if (!base.DoBehavior1()) return false;
+        return true;
+    }
+}
