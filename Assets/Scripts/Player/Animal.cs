da953687eb2655366dcf259253d8f9f41a0f01bf
忆风凌 <1059649765@qq.com>
2026-01-31 11:11:15 +0800

    do

---
+++ Assets/Scripts/Player/Animal.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    public bool isplayer;
+    
+    public void Start()
+    {
+        //播放动画
+        //
+    }
+    
+    //onhit
+    //ondeath
+    //attack
+    //do behavior
+    
+}
